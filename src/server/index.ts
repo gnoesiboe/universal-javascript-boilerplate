2c8f1b2190fb6201372ce30b3fd7d1d94798459d
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2019-02-22 14:02:53 +0100

    tslint integration + some small improvements

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
 import app, { PORT } from './app';
 
 const server = app.listen(PORT, () => {
+    // tslint:disable-next-line: no-console
     console.log(`App is running at http://localhost:${PORT}`);
 });
 
